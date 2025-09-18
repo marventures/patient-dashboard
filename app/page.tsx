c473fb5de1420376f6f3dd24928da2713712f73e
Marvin Pacis <108392678+marventures@users.noreply.github.com>
2025-09-18 19:24:11 +0800

    style: remove jsdoc comments

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,5 @@
 import { redirect } from 'next/navigation';
 
-/**
- * @route RootPage
- * @version 1.0.0
- * @updated 18/09/2025
- * @author Marvin M. Pacis (2025)
- *
- * The `RootPage` route acts as the main entry point of the application and handles initial routing logic.
- * It programmatically redirects users to the `/dashboard` route upon visiting the root path.
- *
- * Features:
- * - Server-side redirection to the main dashboard route.
- *
- * Utilizes:
- * - `redirect` from `next/navigation` to perform a server-side redirect.
- *
- * @async
- * @returns {Promise<void>} This route does not render a UI; it triggers a redirection to `/dashboard`.
- *
- * @example
- * // Visiting `/` will redirect users to `/dashboard`
- * <RootPage />
- */
 const RootPage = async (): Promise<void> => {
   return redirect('/dashboard');
 };
